b775492fd6814f950ac86e9ac9380499a705ec42
alexsit19 <alexsit12@gmail.com>
2022-03-01 11:46:49 +0300

    replace FirstScreen to Stateful widget, and add exitDialog

--- lib/screens/first_screen.dart
+++ lib/screens/first_screen.dart
@@ -1,23 +1,33 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:navigation/screens/second_screen.dart';
 
-class FirstScreen extends StatelessWidget {
+class FirstScreen extends StatefulWidget {
   const FirstScreen({Key? key}) : super(key: key);
 
   @override
+  FirstScreenState createState() => FirstScreenState();
+}
+
+class FirstScreenState extends State<FirstScreen> {
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text("First screen"),
-        ),
-        body: Center(
-          child: ElevatedButton(
-            child: const Text("Go next"),
-            onPressed: () {
-              navigateAndGetData(context);
-            },
+    return WillPopScope(
+      onWillPop: () => showExitDialog(context),
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Text("First screen"),
           ),
-        ));
+          body: Center(
+            child: ElevatedButton(
+              child: const Text("Go next"),
+              onPressed: () {
+                navigateAndGetData(context);
+              },
+            ),
+          )),
+    );
   }
 
   void navigateAndGetData(BuildContext context) async {
@@ -37,6 +47,31 @@ class FirstScreen extends StatelessWidget {
             onPressed: () => Navigator.pop(context, 'OK'),
             child: const Text("OK"))
       ],
+    );
+  }
+  Future<bool> showExitDialog(context) async {
+    return await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Exit dialog"),
+          content: const Text("Do you want to exit?"),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text("NO")
+            ),
+            TextButton(
+                onPressed: () {
+                  exit(0);
+                },
+                child: const Text("YES")
+            ),
+          ]
+        );
+      }
     );
   }
 }
